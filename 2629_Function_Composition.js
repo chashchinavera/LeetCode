75b42f5136c3fe74fe5d8b5ecd4cc0a91efc459a
Vera Chashchina <chaschinavera@gmail.com>
2025-08-18 14:05:55 +0300

    Task 2629 Function Composition done

---
+++ 2629_Function_Composition.js
@@ -0,0 +1,20 @@
+"use strict";
+
+var compose = function (functions) {
+  return function (x) {
+    for (let i = functions.length - 1; i >= 0; i--) {
+      x = functions[i](x);
+    }
+
+    return x;
+  };
+};
+
+const fn1 = compose([(x) => x + 1, (x) => x * x, (x) => 2 * x]);
+console.log(fn1(4)); // 65
+
+const fn2 = compose([(x) => 10 * x, (x) => 10 * x, (x) => 10 * x]);
+console.log(fn2(1)); // 1000
+
+const fn3 = compose([]);
+console.log(fn3(42)); // 42
