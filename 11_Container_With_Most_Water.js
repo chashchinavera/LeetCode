16417dc2c684df25a1bb582c5df4de00e8719f06
Vera Chashchina <chaschinavera@gmail.com>
2025-11-18 19:56:18 +0300

    Task 11 Container With Most Water done

---
+++ 11_Container_With_Most_Water.js
@@ -0,0 +1,33 @@
+"use strict";
+
+var maxArea = function (height) {
+  let maxVolume = -Infinity;
+
+  let left = 0;
+  let right = height.length - 1;
+
+  while (left <= right) {
+    const currentLeft = height[left];
+
+    const currentRight = height[right];
+    const currentVolume = Math.min(currentLeft, currentRight) * (right - left);
+    maxVolume = Math.max(maxVolume, currentVolume);
+    if (right > 0 && left < height.length - 1) {
+      if (height[left] < height[right]) {
+        left += 1;
+      } else {
+        right -= 1;
+      }
+    } else if (right > 0) {
+      right -= 1;
+    } else if (left < height.length - 1) {
+      left += 1;
+    }
+  }
+
+  return maxVolume;
+};
+
+console.log(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7])); //49
+console.log(maxArea([1, 1])); //1
+console.log(maxArea([1, 2, 4, 3])); //4
